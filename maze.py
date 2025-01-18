bd27a0c888c9e8a0283875a06d6c02afd80b46ac
yoelleghesse <yoelleghesse@gmail.com>
2025-01-17 22:24:43 -0500

    tests added and win debug

--- maze.py
+++ maze.py
@@ -11,7 +11,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win=None,
     ):
         self._x1 = x1
         self._y1 = y1
@@ -35,7 +35,7 @@ class Maze:
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
-        if self.__win is None:
+        if self._win is None:
             return
         x1 = self._x1 + i * self._cell_size_x
         y1 = self._y1 + j * self._cell_size_y   
