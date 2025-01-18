41c05dee06e7610c7ad78a1fe59ae65f8be3deeb
yoelleghesse <yoelleghesse@gmail.com>
2025-01-17 22:40:34 -0500

    wall removal and tests

--- tests.py
+++ tests.py
@@ -17,17 +17,17 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
-    def test_maze_create_cells_large(self):
-        num_cols = 16
-        num_rows = 12
+    def test_maze_break_entrance_and_exit(self):
+        num_cols = 12
+        num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
-            len(m1._cells),
-            num_cols,
+            m1._cells[0][0].has_top_wall,
+            False,
         )
         self.assertEqual(
-            len(m1._cells[0]),
-            num_rows,
+            m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
+            False,
         )
 
 
