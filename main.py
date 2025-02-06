41c714cf67988142b140267c55905d6c65d18a2c
yoelleghesse <yoelleghesse@gmail.com>
2025-02-05 19:51:07 -0500

    solve func

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ def main():
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)
 
     maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    print("maze created")
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("Maze is not solvable")
+    else:
+        print("Maze is solvable")   
     win.wait_for_close()
-
 
 main()
 
